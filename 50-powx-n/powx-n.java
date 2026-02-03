52657238d36197c5f0baff85488a7d54539e7a83
QuickGames <QuickGamesDev@gmail.com>
2026-02-03 21:31:02 +0700

    Time: 0 ms (100.00%) | Memory: 48.5 MB (8.95%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        double result = 1d;
+        long nl = Math.abs((long)n);
+        while (0 < nl) {
+            if (nl % 2 == 1) {
+                result *= x;
+                nl--;
+            } else {
+                x *= x;
+                nl /= 2;
+            }
+        }
+        if (n < 0) result = 1 / result;
+        return result;
+    }
+}
