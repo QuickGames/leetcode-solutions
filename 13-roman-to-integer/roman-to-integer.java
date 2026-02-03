4229b9c888e833895842a0ae393059a14c257636
QuickGames <QuickGamesDev@gmail.com>
2026-02-03 09:47:20 +0700

    Time: 4 ms (57.28%) | Memory: 46.5 MB (72.72%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        int buf = 0;
+        int lastCost = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int cost = getCost(s.substring(i, i + 1));
+            if (cost == 1 || cost == 10 || cost == 100 || cost == 1000) {
+                if (lastCost == 0 || lastCost == cost) {
+                    buf += cost;
+                } else {
+                    if (cost < lastCost) {
+                        result += buf;
+                        buf = cost;
+                    } else {
+                        buf = cost - buf;
+                    }
+                }
+                lastCost = cost;
+            } else {
+                if (buf < cost) {
+                    result += (cost - buf);
+                    buf = 0;
+                }
+                else {
+                    result += buf + cost;
+                    buf = 0;
+                }
+                lastCost = 0;
+            }
+        }
+        return result + buf;
+    }
+
+    private int getCost(String symbol) {
+        if (symbol.equals("I")) return 1;
+        else if (symbol.equals("V")) return 5;
+        else if (symbol.equals("X")) return 10;
+        else if (symbol.equals("L")) return 50;
+        else if (symbol.equals("C")) return 100;
+        else if (symbol.equals("D")) return 500;
+        else if (symbol.equals("M")) return 1000;
+        return 0;
+    }
+}
