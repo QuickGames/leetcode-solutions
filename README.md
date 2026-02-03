2bd2d7c40052f70c82666d5d846afa562ec7e940
quickgames <quickgamesdev@gmail.com>
2026-02-03 23:03:46 +0700

    - Structure: init src/test folders - Solved: Task 50

---
+++ README.md
@@ -0,0 +1,5 @@
+# LeetCode Solutions
+
+| # | Task | Difficulty | Topic | Solution | Tests |
+|---|------|------------|-------|----------|-------|
+| 50 | [Pow(x, n)](https://leetcode.com/problems/powx-n/) | Medium | Math, Recursion | [Solution.java](./src/medium/p50_powx_n/Solution.java) | [Tests](./test/medium/p50_powx_n/SolutionTest.java) |
--- src/medium/p50_powx_n/Solution.java
+++ src/medium/p50_powx_n/Solution.java
@@ -1,7 +1,13 @@
+package medium.p50_powx_n;
+
+/**
+ * @author QuickGames
+ * Topic: Math, Recursion
+ */
 class Solution {
     public double myPow(double x, int n) {
         double result = 1d;
-        long nl = Math.abs((long)n);
+        long nl = Math.abs((long) n);
         while (0 < nl) {
             if (nl % 2 == 1) {
                 result *= x;
