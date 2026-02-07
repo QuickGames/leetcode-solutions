acfc6e8178d1e52050ced1b00bc31f597a1cfada
quickgames <quickgamesdev@gmail.com>
2026-02-07 20:42:24 +0700

    Added solved problems from the Main branch

--- src/java_problems/medium/p1756_minimum_deletions_to_make_string_balanced/Solution.java
+++ src/java_problems/medium/p1756_minimum_deletions_to_make_string_balanced/Solution.java
@@ -1,3 +1,13 @@
+package java_problems.medium.p1756_minimum_deletions_to_make_string_balanced;
+
+/**
+ * @author QuickGames
+ * Difficulty: Medium
+ * Number: 1653
+ * Title: Minimum Deletions to Make String Balanced
+ * Language: Java
+ * Topic: Senior, String, Dynamic Programming, Stack, Biweekly Contest 39
+ */
 class Solution {
 
     public int minimumDeletions(String s) {
@@ -14,7 +24,7 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == a) aCount++;
             else aCount--;
-            if (aMax <= aCount){
+            if (aMax <= aCount) {
                 aMax = aCount;
                 iMin = i;
             }
@@ -27,10 +37,10 @@ class Solution {
         int bCount = 0;
         int iMax = s.length();
 
-        for (int i = s.length() - 1; iMin < i ; i--) {
+        for (int i = s.length() - 1; iMin < i; i--) {
             if (s.charAt(i) == b) bCount++;
             else bCount--;
-            if (bMax <= bCount){
+            if (bMax <= bCount) {
                 bMax = bCount;
                 iMax = i;
             }
@@ -39,14 +49,12 @@ class Solution {
             if (s.charAt(i) == a) result++;
         }
 
-        if (iMin + 1 <= iMax) return result;
+        System.out.println("s: " + s);
+        System.out.println("iMin: " + iMin);
+        System.out.println("iMax: " + iMax);
+        System.out.println("result: " + result);
 
         return result;
-    }
-
-    private int min(int a, int b) {
-        if (a < b) return a;
-        else return b;
     }
 
 }
---
+++ test/java_problems/medium/p1756_minimum_deletions_to_make_string_balanced/SolutionTest.java
@@ -0,0 +1,36 @@
+package java_problems.medium.p1756_minimum_deletions_to_make_string_balanced;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+@DisplayName("3634. Minimum Removals to Balance Array")
+class SolutionTest {
+
+    static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of("aababbab", 2),
+                Arguments.of("bbaaaaabb", 2),
+                Arguments.of("baababbaabbaaabaabbabbbabaaaaaabaabababaaababbb", 18),
+                Arguments.of("a", 0),
+                Arguments.of("b", 0),
+                Arguments.of("ababaaaabbbbbaaababbbbbbaaabbaababbabbbbaabbbbaabbabbabaabbbababaa", 25),
+                Arguments.of("aaabbb", 0),
+                Arguments.of("bbbaaa", 3),
+                Arguments.of("ababababab", 4),
+                Arguments.of("bababababa", 5)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideData")
+    void constructTransformedArray(String s, int expected) {
+        Solution solution = new Solution();
+        Assertions.assertEquals(expected, solution.minimumDeletions(s));
+    }
+
+}
