df439f113b1b277ed840264f6cfadbaff388b0b1
QuickGames <QuickGamesDev@gmail.com>
2026-02-19 15:24:21 +0700

    Time: 6 ms (20.72%) | Memory: 48.8 MB (50.22%) - LeetSync

---
+++ 338-counting-bits/counting-bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] result = new int[n+1];
+        for (int i = 0; i <= n; i++){
+            int c = i;
+            while (0 < c) {
+                if ((c & 1) == 1) result[i]++;
+                c >>= 1;
+            }
+        }
+        return result;
+    }
+}
