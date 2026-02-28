82e2eb71b722370d0270562629d99ced4a709c62
QuickGames <QuickGamesDev@gmail.com>
2026-02-28 10:41:43 +0700

    Time: 438 ms (22.09%) | Memory: 42.6 MB (63.95%) - LeetSync

---
+++ 1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    private static final int BITS_LENGTH = 32;
+    private static final int MODULO = 1_000_000_007;
+
+    public int concatenatedBinary(int n) {
+        long result = 0;
+
+        boolean[] bits = new boolean[BITS_LENGTH];
+        for (int i = 1; i <= n; i++) {
+
+            int k = i;
+            int length = 0;
+            while (0 < k) {
+                bits[BITS_LENGTH - length - 1] = ((k & 1) == 1);
+                k >>= 1;
+                length++;
+            }
+
+            int bitsStart = BITS_LENGTH - length ;
+            for (int j = 0; j < length; j++) {
+                result <<= 1;
+                if (bits[bitsStart + j]) result |= 1;
+            }
+            if (MODULO < result) result %= MODULO;
+
+        }
+
+        return (int) result;
+    }
+
+}
