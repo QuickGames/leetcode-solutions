0e90e5118d293b88b35621b6606011f58b8f8680
QuickGames <QuickGamesDev@gmail.com>
2026-03-05 09:41:02 +0700

    Time: 3 ms (92.43%) | Memory: 43.9 MB (45.61%) - LeetSync

--- 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.java
@@ -1,21 +1,8 @@
 class Solution {
-
     public int minOperations(String s) {
-        char[] chars = s.toCharArray();
-        int from1 = 0;
-        int from0 = 0;
-        for (int i = 0; i < chars.length; i++) {
-            boolean isOdd = ((i & 1) == 1);
-            if (isOdd) {
-                from1 += (chars[i] == '0' ? 1 : 0);
-                from0 += (chars[i] == '1' ? 1 : 0);
-            } else {
-                from1 += (chars[i] == '1' ? 1 : 0);
-                from0 += (chars[i] == '0' ? 1 : 0);
-            }
-        }
-
-        return chars.length - (from0 < from1 ? from1 : from0);
+        int length = s.length();
+        int count = 0;
+        for (int i = 0; i < length; i++) count += (i ^ s.charAt(i)) & 1;
+        return Math.min(count, length - count);
     }
-
 }
