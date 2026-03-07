32c16f0897e0425dfd1f29a344edb4dec7260847
QuickGames <QuickGamesDev@gmail.com>
2026-03-08 02:04:16 +0700

    Time: 3 ms (12.03%) | Memory: 46.2 MB (5.08%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -0,0 +1,97 @@
+class Solution {
+
+    public int orangesRotting(int[][] grid) {
+        int result = 0;
+
+        ArrayInt2 rotted = new ArrayInt2();
+        for (int i = 0; i < grid.length; i++)
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) result = -1;
+                else if (grid[i][j] == 2) rotted.add(i, j);
+            }
+
+        if (result == 0) return result;
+
+        ArrayInt2 nextRotted = new ArrayInt2();
+
+        while (0 < rotted.getLength()) {
+
+            for (int i = 0; i < rotted.getLength(); i++) {
+                int[] food = rotted.get(i);
+
+                int row = food[0];
+                int col = food[1];
+
+                int nextRow = row;
+                int nextCol = col;
+
+                nextCol = col - 1;
+                if (0 <= nextCol && grid[nextRow][nextCol] == 1) {
+                    grid[nextRow][nextCol] = 2;
+                    nextRotted.add(nextRow, nextCol);
+                }
+
+                nextCol = col + 1;
+                if (nextCol < grid[row].length && grid[nextRow][nextCol] == 1) {
+                    grid[nextRow][nextCol] = 2;
+                    nextRotted.add(nextRow, nextCol);
+                }
+
+                nextRow = row - 1;
+                nextCol = col;
+                if (0 <= nextRow && grid[nextRow][nextCol] == 1) {
+                    grid[nextRow][nextCol] = 2;
+                    nextRotted.add(nextRow, nextCol);
+                }
+
+                nextRow = row + 1;
+                if (nextRow < grid.length && grid[nextRow][nextCol] == 1) {
+                    grid[nextRow][nextCol] = 2;
+                    nextRotted.add(nextRow, nextCol);
+                }
+
+            }
+
+            rotted.fill(nextRotted);
+            nextRotted.clear();
+            result++;
+        }
+
+        for (int[] ints : grid)
+            for (int anInt : ints)
+                if (anInt == 1) return -1;
+
+        return result;
+    }
+
+    private static class ArrayInt2 {
+
+        private final int[][] nums = new int[100][2];
+        private int length = 0;
+
+        public int getLength() {
+            return length;
+        }
+
+        int[] get(int index) {
+            return new int[]{nums[index][0], nums[index][1]};
+        }
+
+        void add(int row, int col) {
+            nums[length] = new int[]{row, col};
+            length++;
+        }
+
+        void fill(ArrayInt2 values) {
+            clear();
+            for (int i = 0; i < values.length; i++)
+                add(values.nums[i][0], values.nums[i][1]);
+        }
+
+        void clear() {
+            length = 0;
+        }
+
+    }
+
+}
