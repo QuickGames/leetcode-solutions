2ab74af2ca586e6408f4a26adb6e66b896320dda
QuickGames <QuickGamesDev@gmail.com>
2026-03-12 01:16:18 +0700

    Time: 16 ms (98.48%) | Memory: 50.9 MB (85.47%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+        int result = 0;
+
+        Map<Integer, boolean[]> seats = new HashMap<>();
+        for (int[] reservedSeat : reservedSeats) {
+            boolean[] currentSeats = seats.getOrDefault(reservedSeat[0], null);
+            if (currentSeats == null) {
+                currentSeats = new boolean[10];
+                seats.put(reservedSeat[0], currentSeats);
+            }
+            currentSeats[reservedSeat[1] - 1] = true;
+        }
+
+        for (Map.Entry<Integer, boolean[]> row : seats.entrySet()) {
+            boolean[] seat = row.getValue();
+            if (!seat[1] && !seat[2] && !seat[3] && !seat[4]) {
+                result++;
+                if (!seat[5] && !seat[6] && !seat[7] && !seat[8])
+                    result++;
+            } else if (!seat[3] && !seat[4] && !seat[5] && !seat[6])
+                result++;
+            else if (!seat[5] && !seat[6] && !seat[7] && !seat[8])
+                result++;
+
+        }
+
+        int emptyRows = n - seats.size();
+        result += emptyRows * 2;
+
+        return result;
+    }
+
+}
