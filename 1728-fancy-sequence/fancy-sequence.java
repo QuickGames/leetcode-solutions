48b9f814e24332de7fca4d1eaa5b5f3a82438592
QuickGames <QuickGamesDev@gmail.com>
2026-03-15 14:05:08 +0700

    Time: 1897 ms (5.17%) | Memory: 128.4 MB (31.03%) - LeetSync

---
+++ 1728-fancy-sequence/fancy-sequence.java
@@ -0,0 +1,45 @@
+class Fancy {
+
+    private static final int MODULO = 1_000_000_007;
+
+    private final int[] values;
+    private int valuesCount;
+
+    public Fancy() {
+        values = new int[100_000];
+        valuesCount = 0;
+    }
+
+    public void append(int val) {
+        values[valuesCount++] = val;
+    }
+
+    public void addAll(int inc) {
+        for (int i = 0; i < valuesCount; i++) {
+            long val = (long) values[i] + inc;
+            values[i] = (int) (val < MODULO ? val : (val % MODULO));
+        }
+    }
+
+    public void multAll(int m) {
+        for (int i = 0; i < valuesCount; i++) {
+            long val = (long) values[i] * m;
+            values[i] = (int) (val % MODULO);
+        }
+    }
+
+    public int getIndex(int idx) {
+        if (valuesCount <= idx) return -1;
+        return values[idx];
+    }
+
+}
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy obj = new Fancy();
+ * obj.append(val);
+ * obj.addAll(inc);
+ * obj.multAll(m);
+ * int param_4 = obj.getIndex(idx);
+ */
