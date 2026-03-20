95e5e1ac2722edff3d2726e007951acf7d62279e
QuickGames <QuickGamesDev@gmail.com>
2026-03-21 04:00:51 +0700

    Time: 181 ms (19.61%) | Memory: 43.5 MB (77.25%) - LeetSync

---
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public String longestPalindrome(String s) {
+        String result = "";
+        int resultLength = 0;
+
+        for (int left = 0; left < s.length(); left++) {
+            for (int right = s.length() - 1; left <= right && resultLength < right - left + 1; right--) {
+
+                int subLength = right - left + 1;
+                boolean isPalindrome = true;
+                for (int i = 0; i < subLength / 2; i++) {
+                    if (s.charAt(left + i) != s.charAt(right - i)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                if (isPalindrome && resultLength < subLength) {
+                    result = s.substring(left, right + 1);
+                    resultLength = subLength;
+                }
+
+            }
+        }
+
+        return result;
+    }
+
+}
