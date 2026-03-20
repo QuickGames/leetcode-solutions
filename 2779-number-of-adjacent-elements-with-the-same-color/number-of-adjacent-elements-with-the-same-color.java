dde5dec062b8e59c31628201d0b4c3ea10080774
QuickGames <QuickGamesDev@gmail.com>
2026-03-20 23:11:42 +0700

    Time: 4 ms (100.00%) | Memory: 207.9 MB (89.60%) - LeetSync

---
+++ 2779-number-of-adjacent-elements-with-the-same-color/number-of-adjacent-elements-with-the-same-color.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    public int[] colorTheArray(int n, int[][] queries) {
+        if (queries.length == 1) return new int[]{0};
+
+        int[] colors = new int[n];
+
+        int[] result = new int[queries.length];
+        int lastCount = 0;
+        for (int i = 0; i < queries.length; i++) {
+            int count = lastCount;
+
+            int[] query = queries[i];
+            int qIndex = query[0];
+            int qColor = query[1];
+            if (colors[qIndex] != 0) {
+                if (0 < qIndex && colors[qIndex - 1] == colors[qIndex]) count--;
+                if (qIndex + 1 < n && colors[qIndex] == colors[qIndex + 1]) count--;
+            }
+
+            if (0 < qIndex && colors[qIndex - 1] == qColor) count++;
+            if (qIndex + 1 < n && qColor == colors[qIndex + 1]) count++;
+
+            colors[qIndex] = qColor;
+
+            lastCount = count;
+            result[i] = count;
+        }
+        return result;
+    }
+
+}
