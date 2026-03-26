a7dbc540e8a4f871da4a97d693e19a0f02c48198
QuickGames <QuickGamesDev@gmail.com>
2026-03-26 19:17:09 +0700

    Time: 5 ms (56.62%) | Memory: 46.5 MB (84.96%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        StringBuilder[] sb = new StringBuilder[numRows];
+        for (int i = 0; i < sb.length; i++)
+            sb[i] = new StringBuilder();
+
+        int index = 0;
+        boolean direction = true;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            sb[index].append(c);
+            if (1 < numRows) {
+                index += (direction ? 1 : -1);
+                if (index == 0) direction = true;
+                else if (index == numRows - 1) direction = false;
+            }
+        }
+
+        StringBuilder result = new StringBuilder(numRows);
+        for (StringBuilder sbi : sb) result.append(sbi);
+        return result.toString();
+    }
+}
