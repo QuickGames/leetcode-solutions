b14a1748232d934d949ad8bd907541a3698a3297
QuickGames <QuickGamesDev@gmail.com>
2026-03-28 11:51:16 +0700

    Time: 0 ms (100.00%) | Memory: 9.3 MB (29.07%) - LeetSync

--- 69-sqrtx/sqrtx.c
+++ 69-sqrtx/sqrtx.c
@@ -5,12 +5,11 @@ int mySqrt(int x) {
 
     while (1 < right - left) {
         int current = (right + left - 1) / 2 + 1;
-        printf("cur: %d; left: %d; right: %d\n", current, left, right);
         int c2 = current * current;
         if (c2 < x) left = current;
         else if (x < c2) right = current;
         else return current;
     }
-    
+
     return left;
 }
